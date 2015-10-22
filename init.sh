3cab6538451b73281b24a916909a9b8a284ce7c5
raimusyndrome <dummy@example.com>
2015-10-22 15:45:29 +0900

    テンプレートのディレクトリ構成をサポートするための前準備

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+# home directoryで実行する
+
 REPO_DIR=.vim/vimconf
 cd ~
 ln -s $REPO_DIR/vimrc .vimrc
+ln -s $REPO_DIR/gvimrc .gvimrc
+
+# デフォルトのディレクトリ作成
+# mkdir ~/vimdata
+# mkdir ~/backup
+# mkdir ~/undo
 
